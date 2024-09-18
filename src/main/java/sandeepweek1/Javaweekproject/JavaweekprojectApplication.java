18c471c19d1285520c323f67b2ee5f14a2ab24f8
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-09-18 07:04:29 +0530

    Day 2 Learning - Beans

---
+++ src/main/java/sandeepweek1/Javaweekproject/JavaweekprojectApplication.java
@@ -0,0 +1,16 @@
+package sandeepweek1.Javaweekproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaweekprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaweekprojectApplication.class, args);
+	}
+
+
+
+
+}
---
+++ src/test/java/sandeepweek1/Javaweekproject/JavaweekprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package sandeepweek1.Javaweekproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaweekprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
