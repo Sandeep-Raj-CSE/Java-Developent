3f570e2b0bd7a351926bffbb74b39cedf1eca873
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-09-21 16:55:19 +0530

    Add Week 1 content

---
+++ week2springboot/Springboot-web/src/main/java/com/week2learning/Springboot/web/SpringbootWebAndRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.week2learning.Springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebAndRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebAndRestApiApplication.class, args);
+	}
+
+}
---
+++ week2springboot/Springboot-web/src/test/java/com/week2learning/Springboot/web/SpringbootWebAndRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2learning.Springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebAndRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
